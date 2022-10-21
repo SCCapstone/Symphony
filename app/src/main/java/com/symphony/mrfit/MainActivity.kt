a1dda2a0a003049c080ca31a134e631a61d8229e
NikM3 <nikolas.a.melendez@gmail.com>
2022-10-21 17:30:34 -0400

    Testing for conflict

--- app/src/main/java/com/symphony/mrfit/MainActivity.kt
+++ app/src/main/java/com/symphony/mrfit/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
         /* Change this section merge conflict */
         val greeting = findViewById<TextView>(R.id.helloTextView)
-        greeting.text = "Hello Nik"
+        greeting.text = "Hello Nik, again"
         /* Change this section merge conflict */
     }
 }
