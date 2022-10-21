32fe4b072bc0591cbfe988459290ea3cf177a3fe
esartawi <esartawi@email.sc.edu>
2022-10-21 17:29:43 -0400

    my edits

--- app/src/main/java/com/symphony/mrfit/MainActivity.kt
+++ app/src/main/java/com/symphony/mrfit/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
         /* Change this section merge conflict */
         val greeting = findViewById<TextView>(R.id.helloTextView)
-        greeting.text = "Hello Nik"
+        greeting.text = "Hello Esam"
         /* Change this section merge conflict */
     }
 }
