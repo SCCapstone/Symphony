8516de8068b69d9127be80fce4e808af983686f3
NikM3 <nikolas.a.melendez@gmail.com>
2022-10-21 00:38:19 -0400

    Init commit

---
+++ app/src/main/java/com/symphony/mrfit/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.symphony.mrfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /* Change this section merge conflict */
+        val greeting = findViewById<TextView>(R.id.helloTextView)
+        greeting.text = "Hello Nik"
+        /* Change this section merge conflict */
+    }
+}
