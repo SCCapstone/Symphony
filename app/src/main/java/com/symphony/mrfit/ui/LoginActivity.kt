ce9f1f6cf887a7b0df6ed7c1b5acc06c58db3ee6
NikM3 <nikolas.a.melendez@gmail.com>
2022-11-01 14:39:19 -0400

    Updated gradle files for Firebase Authentication and refactored Activities into UI package

---
+++ app/src/main/java/com/symphony/mrfit/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.symphony.mrfit.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.symphony.mrfit.R
+
+class loginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
