2fe71991a56393e78c2eca873be3545292e6d621
NikM3 <nikolas.a.melendez@gmail.com>
2022-11-10 23:40:28 -0500

    Added Copyright

--- app/src/main/java/com/symphony/mrfit/data/login/LoginRepository.kt
+++ app/src/main/java/com/symphony/mrfit/data/login/LoginRepository.kt
@@ -1,3 +1,9 @@
+/*
+ * Created by Team Symphony 11/10/22, 11:39 PM
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 11/10/22, 11:38 PM
+ */
+
 package com.symphony.mrfit.data.login
 
 
