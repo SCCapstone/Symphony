90dbe89430adf3886806603aaf47689eb9d49c75
Nicole Kate Calderon <ngc2@email.sc.edu>
2022-11-28 15:41:33 -0500

    editing calendar #14

---
+++ app/src/main/java/com/symphony/mrfit/ui/Calendar.kt
@@ -0,0 +1,35 @@
+package com.symphony.mrfit.ui
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.CalendarView.OnDateChangeListener
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.symphony.mrfit.R
+
+class Calendar : AppCompatActivity() {
+
+    // variables for text view and calendar view
+    lateinit var date: TextView
+    lateinit var calendarView: CalendarView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+
+        date = findViewById(R.id.idTVDate)
+        calendarView = findViewById(R.id.calendarView)
+
+        calendarView
+            .setOnDateChangeListener(
+                OnDateChangeListener { calendarView, year, month, dayOfMonth ->
+                    val Date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+
+                    date.setText(Date)
+                })
+
+    }
+}
