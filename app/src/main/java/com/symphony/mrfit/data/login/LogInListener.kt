9cc6b8122b75ab5ea454c359961a85e386f1fa8f
NikM3 <nikolas.a.melendez@gmail.com>
2023-02-02 16:46:36 -0500

    Empty login bug fix, close #67

--- app/src/main/java/com/symphony/mrfit/data/login/LogInListener.kt
+++
@@ -1,6 +0,0 @@
-package com.symphony.mrfit.data.login
-
-public interface LogInListener  {
-    fun logInSuccess(email: String?, password: String?)
-    fun logInFailure(exception: Exception?, email: String?, password: String?)
-}
