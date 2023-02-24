0c6bd55dc817bf59b34128f2e968533354c2a311
Nicole Kate Calderon <ngc2@email.sc.edu>
2023-02-24 17:04:54 -0500

    branch

--- app/src/main/java/com/symphony/mrfit/ui/NotifChannel.kt
+++ app/src/main/java/com/symphony/mrfit/ui/NotifChannel.kt
@@ -1,53 +1,38 @@
 //package com.symphony.mrfit.ui
 //
-//import android.app.*
+//import android.app.Notification
+//import android.app.NotificationChannel
+//import android.app.NotificationManager
+//import android.content.BroadcastReceiver
 //import android.content.Context
 //import android.content.Intent
-//import android.media.RingtoneManager
 //import android.os.Build
-//import androidx.appcompat.app.AppCompatActivity
-//import android.os.Bundle
-//import android.util.Log
-//import androidx.annotation.RequiresApi
-//import androidx.core.app.NotificationCompat
-//import androidx.core.app.NotificationManagerCompat
-//import com.symphony.mrfit.R
-//import com.symphony.mrfit.databinding.ActivityMainBinding
-//import java.util.*
+//import com.symphony.mrfit.ui.MainActivity.NOTIFICATION_CHANNEL_ID
 //
-//
-////for scheduling notifs
-//class AlarmReceiver : NotifChannel() {
-//
-//    override fun onReceive(context: Context, intent: Intent?) {
-//        Log.d("this", "notify")
-//
+////publishes the notification
+//class NotifChannel : BroadcastReceiver() {
+//    override fun onReceive(context: Context, intent: Intent) {
+//        val notificationManager =
+//            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        val notification = intent.getParcelableExtra<Notification>(NOTIFICATION)
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//
-//            val intent = Intent(context, NotificationActivity::class.java)
-//            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-//            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
-//
-//            val builder = NotificationCompat.Builder(context, "111")
-//                .setSmallIcon(R.drawable)
-//                .setContentTitle("Alarm is running")
-//                .setAutoCancel(true)
-//                .setDefaults(NotificationCompat.DEFAULT_ALL)
-//                .setDefaults(NotificationCompat.DEFAULT_SOUND)
-//                .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
-//                .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                .addAction(R.drawable.ic_baseline_stop_24,"Stop",pendingIntent)
-//                .setContentIntent(pendingIntent)
-//
-//            val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-//            val r = RingtoneManager.getRingtone(context, notification)
-//            r.play()
-//
-//            val notificationManagerCompat = NotificationManagerCompat.from(context)
-//            notificationManagerCompat.notify(123, builder.build())
-//
+//            val importance = NotificationManager.IMPORTANCE_HIGH
+//            val notificationChannel = NotificationChannel(
+//                NOTIFICATION_CHANNEL_ID,
+//                "NOTIFICATION_CHANNEL_NAME",
+//                importance
+//            )
+//            assert(notificationManager != null)
+//            notificationManager.createNotificationChannel(notificationChannel)
 //        }
-//
+//        val id = intent.getIntExtra(NOTIFICATION_ID, 0)
+//        assert(notificationManager != null)
+//        notificationManager.notify(id, notification)
 //    }
 //
+//    companion object {
+//        const val NOTIFICATION_CHANNEL_ID = "10001"
+//        const val default_notification_channel_id = "default"
+//    }
 //}
+
