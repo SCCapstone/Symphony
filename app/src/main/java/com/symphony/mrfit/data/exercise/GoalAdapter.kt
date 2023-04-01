f4f0cc30bd08453c8185ea3ca0a01c45354d91bc
NikM3 <nikolas.a.melendez@gmail.com>
2023-04-01 15:10:43 -0400

    Initial goals commit

--- app/src/main/java/com/symphony/mrfit/data/exercise/GoalAdapter.kt
+++ app/src/main/java/com/symphony/mrfit/data/exercise/GoalAdapter.kt
@@ -1,28 +1,28 @@
 /*
- *  Created by Team Symphony on 4/1/23, 5:08 AM
+ *  Created by Team Symphony on 4/1/23, 3:10 PM
  *  Copyright (c) 2023 . All rights reserved.
- *  Last modified 4/1/23, 5:08 AM
+ *  Last modified 4/1/23, 3:10 PM
  */
 
 package com.symphony.mrfit.data.exercise
 
 import android.content.Context
-import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
-import androidx.annotation.RequiresApi
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.symphony.mrfit.R
 import com.symphony.mrfit.data.model.Goal
-import java.util.*
+import java.text.DecimalFormat
 
 /**
  * Adapter for dynamically populating a card_goal with a passed list of Histories
  */
 
-class GoalAdapter(val context: Context, val data: ArrayList<Goal>) :
+class GoalAdapter(val context: Context, val data: ArrayList<Goal>, val delete: (String) -> Unit) :
     RecyclerView.Adapter<GoalAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
@@ -32,25 +32,47 @@ class GoalAdapter(val context: Context, val data: ArrayList<Goal>) :
         return ViewHolder(v)
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+
     override fun onBindViewHolder(holder: ViewHolder, i: Int) {
-        holder.goalTitle.text = data[i].name
+        holder.goalName.text = data[i].name
+        holder.goalProgress.text = buildString(i)
+
+        holder.deleteButton.setOnClickListener {
+            delete(data[i].goalID!!)
+            data.removeAt(i)
+            notifyItemRemoved(i)
+            notifyItemRangeChanged(i, itemCount)
+        }
+
+        holder.itemView.setOnClickListener {
+            Toast.makeText(context, "What happens when I press this", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    inner class ViewHolder(histView: View) : RecyclerView.ViewHolder(histView) {
+    inner class ViewHolder(goalView: View) : RecyclerView.ViewHolder(goalView) {
 
-        var goalTitle: TextView
-        var goalTimestamp: TextView
-        var goalDuration: TextView
+        var goalName: TextView
+        var goalProgress: TextView
+        var deleteButton: Button
 
         init {
-            goalTitle = histView.findViewById(R.id.goalNameTextView)
-            goalTimestamp = histView.findViewById(R.id.goalTimestamp)
-            goalDuration = histView.findViewById(R.id.goalDurationTextView)
+            goalName = goalView.findViewById(R.id.goalName)
+            goalProgress = goalView.findViewById(R.id.goalProgress)
+            deleteButton = goalView.findViewById(R.id.deleteGoalButton)
         }
+    }
+
+    private fun buildString(i: Int): String {
+        val format = DecimalFormat("0.#")
+        return context.getString(
+            R.string.goal_format,
+            format.format(data[i].progress),
+            format.format(data[i].endGoal),
+            data[i].quantifier
+        )
     }
 }
--- app/src/main/java/com/symphony/mrfit/ui/GoalsActivity.kt
+++ app/src/main/java/com/symphony/mrfit/ui/GoalsActivity.kt
@@ -1,24 +1,32 @@
 /*
- *  Created by Team Symphony on 4/1/23, 5:08 AM
+ *  Created by Team Symphony on 4/1/23, 3:10 PM
  *  Copyright (c) 2023 . All rights reserved.
- *  Last modified 4/1/23, 5:08 AM
+ *  Last modified 4/1/23, 3:10 PM
  */
 
 package com.symphony.mrfit.ui
 
+import android.app.Dialog
 import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.symphony.mrfit.R
 import com.symphony.mrfit.data.exercise.GoalAdapter
+import com.symphony.mrfit.data.model.Goal
 import com.symphony.mrfit.data.profile.ProfileViewModel
 import com.symphony.mrfit.data.profile.ProfileViewModelFactory
 import com.symphony.mrfit.databinding.ActivityGoalsBinding
+import com.symphony.mrfit.ui.Helper.ZERO
 
 class GoalsActivity : AppCompatActivity() {
 
@@ -40,23 +48,89 @@ class GoalsActivity : AppCompatActivity() {
         super.onStart()
 
         val goalsList = binding.goalsList
+        val newGoal = binding.addGoalButton
         val spinner = binding.loadingSpinner
+
+        fun deleteGoal(goalID: String) {
+            profileViewModel.deleteGoal(goalID)
+        }
 
         spinner.visibility = View.VISIBLE
 
-        //Get data of current User and populate the page
+        // Get data of current User and populate the page
         profileViewModel.getNotifications()
 
         // Set the layout of the list of workouts presented to the user
         layoutManager = LinearLayoutManager(this)
         goalsList.layoutManager = layoutManager
 
+        // Populate the list
+        profileViewModel.getGoals()
         profileViewModel.goals.observe(this, Observer {
             Log.d(ContentValues.TAG, "Filling in username")
             val goals = it ?: return@Observer
 
-            goalsList.adapter = GoalAdapter(this, goals)
+            goalsList.adapter = GoalAdapter(this, goals, ::deleteGoal)
             spinner.visibility = View.GONE
         })
+
+        newGoal.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.popup_new_goal)
+            dialog.setTitle("New Goal")
+
+            val name = dialog.findViewById<EditText>(R.id.editGoalName)
+            val num = dialog.findViewById<EditText>(R.id.goalNumber)
+            val dropdown = dialog.findViewById<Spinner>(R.id.goalSpinner)
+            val other = dialog.findViewById<EditText>(R.id.otherInput)
+            val save = dialog.findViewById<Button>(R.id.saveGoalButton)
+            val cancel = dialog.findViewById<Button>(R.id.cancelGoalButton)
+
+            dropdown.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position == dropdown.adapter.count - 1) {
+                        other.visibility = View.VISIBLE
+                    } else {
+                        other.visibility = View.GONE
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+            }
+
+            save.setOnClickListener {
+                val newName = name.text.toString()
+                val newEnd = num.text.toString().toDouble()
+                var newType = dropdown.selectedItem.toString()
+                if (newType == "Other") {
+                    newType = other.text.toString()
+                }
+
+                profileViewModel.addGoal(
+                    Goal(
+                        newName,
+                        ZERO.toDouble(),
+                        newEnd,
+                        newType
+                    )
+                )
+
+                profileViewModel.getGoals()
+                dialog.dismiss()
+            }
+
+            cancel.setOnClickListener {
+                dialog.dismiss()
+            }
+
+            dialog.show()
+        }
     }
 }
