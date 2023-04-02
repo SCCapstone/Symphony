ca0927ec5000c92a08cf61f7e3a9946fc7c2147e
NikM3 <nikolas.a.melendez@gmail.com>
2023-04-02 00:52:47 -0400

    Post-workout dialog added, close #106

--- app/src/main/java/com/symphony/mrfit/ui/CurrentWorkoutActivity.kt
+++ app/src/main/java/com/symphony/mrfit/ui/CurrentWorkoutActivity.kt
@@ -1,16 +1,19 @@
 /*
- *  Created by Team Symphony on 4/1/23, 10:04 PM
+ *  Created by Team Symphony on 4/2/23, 12:52 AM
  *  Copyright (c) 2023 . All rights reserved.
- *  Last modified 4/1/23, 8:42 PM
+ *  Last modified 4/2/23, 12:52 AM
  */
 
 package com.symphony.mrfit.ui
 
+import android.app.Dialog
 import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.os.SystemClock
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -18,6 +21,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.Timestamp
+import com.symphony.mrfit.R
 import com.symphony.mrfit.data.exercise.ExerciseViewModel
 import com.symphony.mrfit.data.exercise.ExerciseViewModelFactory
 import com.symphony.mrfit.data.exercise.WorkoutAdapter2
@@ -25,9 +29,11 @@ import com.symphony.mrfit.data.model.History
 import com.symphony.mrfit.data.profile.ProfileViewModel
 import com.symphony.mrfit.data.profile.ProfileViewModelFactory
 import com.symphony.mrfit.databinding.ActivityCurrentWorkoutBinding
+import com.symphony.mrfit.ui.Helper.ZERO
 import com.symphony.mrfit.ui.RoutineSelectionActivity.Companion.EXTRA_LIST
 import com.symphony.mrfit.ui.RoutineSelectionActivity.Companion.EXTRA_STRING
 import com.symphony.mrfit.ui.WorkoutRoutineActivity.Companion.EXTRA_ROUTINE
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -97,11 +103,12 @@ class CurrentWorkoutActivity : AppCompatActivity() {
 
         finishButton.setOnClickListener {
             timer.stop()
+            val timeSpent = SystemClock.elapsedRealtime() - timer.base
             profileViewModel.addWorkoutToHistory(
                 History(
                     passedRoutineName!!,
                     Timestamp(Date()),
-                    SystemClock.elapsedRealtime() - timer.base,
+                    timeSpent,
                     passedRoutineID
                 )
             )
@@ -110,12 +117,82 @@ class CurrentWorkoutActivity : AppCompatActivity() {
                 "Your workout has been saved to your history",
                 Toast.LENGTH_SHORT
             ).show()
-            val intent = Intent(applicationContext, HomeActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+
+
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.activity_post_workout)
+            dialog.setTitle("Post Workout")
+
+            val goals = dialog.findViewById<Button>(R.id.gotoGoalsButton)
+            val home = dialog.findViewById<Button>(R.id.returnHomeButton)
+            val text = dialog.findViewById<TextView>(R.id.postWorkoutTime)
+            val startTime = SimpleDateFormat(
+                "'You started this workout at' hh:mm a",
+                Locale.getDefault()
+            )
+                .format(Date().time - timeSpent)
+            val endTime = SimpleDateFormat(
+                "' and finished at ' hh:mm a",
+                Locale.getDefault()
+            )
+                .format(Date())
+            var totalTime = " for a total of "
+            var hours: Long = 0
+            var minutes = timeSpent / 1000 / 60
+            val seconds = timeSpent / 1000 % 60
+            if (minutes >= 60) {
+                hours = minutes % 60
+                minutes /= 60
+            }
+
+            if (hours > ZERO) {
+                totalTime += "$hours hours and $minutes minutes "
+            } else if (minutes > ZERO) {
+                totalTime += "$minutes minutes and "
+            }
+            totalTime += "$seconds seconds."
+
+            text.text = startTime + endTime + totalTime
+
+            dialog.setOnCancelListener {
+                gotoHome()
+            }
+
+            home.setOnClickListener {
+                gotoHome()
+            }
+
+            goals.setOnClickListener {
+                gotoGoals()
+            }
+
+            dialog.show()
         }
+    }
+
+    private fun gotoHome() {
+        val intent = Intent(applicationContext, HomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+
+    private fun gotoGoals() {
+        val homeIntent = Intent(applicationContext, HomeActivity::class.java)
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        homeIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        startActivity(homeIntent)
+
+        val userIntent = Intent(applicationContext, UserProfileActivity::class.java)
+        userIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        startActivity(userIntent)
+
+        val goalIntent = Intent(applicationContext, GoalsActivity::class.java)
+        startActivity(goalIntent)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
---
+++ app/src/main/java/com/symphony/mrfit/ui/PostWorkoutActivity.kt
@@ -0,0 +1,18 @@
+/*
+ *  Created by Team Symphony on 4/2/23, 12:52 AM
+ *  Copyright (c) 2023 . All rights reserved.
+ *  Last modified 4/2/23, 12:52 AM
+ */
+
+package com.symphony.mrfit.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.symphony.mrfit.R
+
+class PostWorkoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_workout)
+    }
+}
