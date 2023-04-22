35eda03af581b3474fe60912598d54604367cf75
nicolekaate <NGC2@email.sc.edu>
2023-04-22 11:42:52 -0400

    edited goals unit test #75

--- app/src/androidTest/java/com/symphony/mrfit/ui/GoalsActivityTest.kt
+++ app/src/androidTest/java/com/symphony/mrfit/ui/GoalsActivityTest.kt
@@ -6,7 +6,11 @@
 
 package com.symphony.mrfit.ui
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.activityScenarioRule
+import com.symphony.mrfit.R
 import org.junit.Rule
 import org.junit.Test
 
@@ -14,9 +18,23 @@ class GoalsActivityTest {
     @get:Rule
     var activityScenarioRule = activityScenarioRule<GoalsActivity>()
 
-    //TODO
+    //TODO: added simple tests, worked on mine but check if needs more
+    //tests if the activity is displayed and visible to user
     @Test
     fun checkActivityVisibility() {
+        Espresso.onView(ViewMatchers.withId(R.id.layout_goalsActivity))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    //tests if components are visible
+    @Test
+    fun checkViewVisibility() {
+        Espresso.onView(ViewMatchers.withId(R.id.goalsTitleTextView))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+        Espresso.onView(ViewMatchers.withId(R.id.goalsList))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
     }
 }
 
